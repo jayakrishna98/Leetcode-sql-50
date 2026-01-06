ce6ada1fa18bf69a3df430aa678a668278d579d3
Jaya Krishna <g.reddy@kantar.com>
2026-01-06 19:34:20 +0530

    Added code comments

--- 1.sql
+++ 1.sql
@@ -1,2 +1,5 @@
+-- Since only distinct product IDs are required, the SELECT statement includes only the product_id column.
+-- A WHERE clause is applied to filter product IDs that are both low fat and recyclable.
+
 select distinct product_id from products
 where low_fats='Y' and recyclable='Y'
